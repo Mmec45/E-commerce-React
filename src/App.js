377b20a523de1db23d85e41ebab7fe31cb36ae34
Cyrille Misse <72013151+Mmec45@users.noreply.github.com>
2022-07-16 12:26:15 +0100

    my code

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ const Shop = () => {
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path='sign-in' element={<SignIn />} />
+        </Route>
+      </Routes>
+
   );
 };
 
