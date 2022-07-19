1b5f5961e5faf41d70b4b8619f4ca7d6a7cc4ff6
Cyrille Misse <72013151+Mmec45@users.noreply.github.com>
2022-07-19 15:02:15 +0100

    update folder

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,43 +1,53 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+// importation de la phase d'authentication de firebase
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+// imprtation de la bd creer dans fireStrom
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAd3rwGvPpAKb4JfrD1LD3BR6almWVcsD0",
+  authDomain: "e-commerce-react-69230.firebaseapp.com",
+  projectId: "e-commerce-react-69230",
+  storageBucket: "e-commerce-react-69230.appspot.com",
+  messagingSenderId: "16261959785",
+  appId: "1:16261959785:web:d534aff4b2a128231cd2f8"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
 
+// fournisseur google Earth
 const provider = new GoogleAuthProvider();
 
+// initaliser les differents types styles de configurations de google
 provider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
+//import l'authenticaation de google vers firebase
 export const auth = getAuth();
+// import l'authentication par popup
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+// obtenir la db contenue dans firestore
 export const db = getFirestore();
-
+// recoit l'objet d'Auth de l'user
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
-
+  // snapshot est une instance de donnée et de voir si elle exsite ou pas et d'entre de la bd 
   const userSnapshot = await getDoc(userDocRef);
-
+  // verifier les donneés existent et avoir le nom et l'emailet creer une nouvelle date
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
+    // obtenir les donnés de l'user si oui
     try {
       await setDoc(userDocRef, {
         displayName,
